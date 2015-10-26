fe54d5cf5f6b93745c030c5dad6ecc53793eeea1
Jack Somervell <jacksomervell@gmail.com>
2015-10-26 21:25:43 +0000

    added all ingredients to page via array and ng repeat

--- frontEnd/js/juiceController.js
+++ frontEnd/js/juiceController.js
@@ -10,7 +10,25 @@ angular
     self.ndbno = ''
 
 
-    this.getData = function() {
+    self.fruitList = [
+    {'name': 'apple', 'image': 'images/apple.gif', 'nutritentNo': 09502 },
+    {'name': 'banana', 'image': 'images/banana.png', 'nutritentNo': 09040 },
+    {'name': 'beetroot', 'image': 'images/beet.png', 'nutritentNo': 11080 },
+    {'name': 'blackberry', 'image': 'images/blackberry.jpeg', 'nutritentNo': 09042 },
+    {'name': 'broccoli', 'image': 'images/broccoli.png', 'nutritentNo': 11090 },
+    {'name': 'cabbage', 'image': 'images/cabbage.jpeg', 'nutritentNo': 11749 },
+    {'name': 'carrot', 'image': 'images/carrot.png', 'nutritentNo': 11124 },
+    {'name': 'celery', 'image': 'images/celery.jpg', 'nutritentNo': 11143 },
+    {'name': 'ginger', 'image': 'images/ginger.png', 'nutritentNo': 11216 },
+    {'name': 'lemon', 'image': 'images/lemon.jpeg', 'nutritentNo': 09150 },
+    {'name': 'lime', 'image': 'images/lime.png', 'nutritentNo': 09159 },
+    {'name': 'orange', 'image': 'images/orange.png', 'nutritentNo': 09200 },
+    {'name': 'pear', 'image': 'images/pear.jpeg', 'nutritentNo': 09252 },
+    {'name': 'strawberry', 'image': 'images/strawberry.png', 'nutritentNo': 09316 }
+
+    ]
+
+    self.getData = function() {
 
      $http
       .get('http://localhost:3000/juices/' + self.ndbno)
@@ -19,7 +37,7 @@ angular
       });
     }
 
-    this.onDrop = function (data, event) {
+    self.onDrop = function (data, event) {
     // Get custom object data.
     var customObjectData = data['json/custom-object']; 
 
@@ -29,7 +47,7 @@ angular
 
   this.testDrop = function () {
     $( "#carrot" ).css( "visibility", "hidden" )
-       $( "#juicer" ).effect( "shake" )
+       $( "#juicer" ).effect( "shake", {times:35, distance:5}, 2000 )
     console.log('fish');
   }
 
